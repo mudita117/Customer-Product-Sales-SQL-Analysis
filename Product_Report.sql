893f40cd7b0751231ea648824c6bc76c1768b644
Mudita Srivastava <muditasrivastava01@gmail.com>
2026-05-28 15:25:12 +0530

    Add files via upload

---
+++ Product_Report.sql
@@ -0,0 +1,78 @@
+/*
+===============================================================================
+Product Report
+===============================================================================
+Purpose:
+    - This report consolidates key product metrics and behaviors.
+
+Highlights:
+    1. Gathers essential fields such as product name, category, subcategory, and cost.
+    2. Segments products by revenue to identify High-Performers, Mid-Range, or Low-Performers.
+    3. Aggregates product-level metrics:
+       - total orders
+       - total sales
+       - total quantity sold
+       - total customers (unique)
+       - lifespan (in months)
+    4. Calculates valuable KPIs:
+       - recency (months since last sale)
+       - average order revenue (AOR)
+       - average monthly revenue
+===============================================================================
+*/
+CREATE VIEW products_report AS 
+WITH base_query AS (
+SELECT
+f.order_number,
+f.order_date,
+f.customer_key,
+f.sales_amount,
+f.quantity,
+p.product_key,
+p.product_name,
+p.category,
+p.subcategory,
+p.cost
+FROM fact_sales f LEFT JOIN
+dim_products p ON 
+f.product_key = p.product_key
+WHERE order_date IS NOT NULL)
+,
+product_aggregations AS (
+SELECT 
+product_key,
+product_name,
+category,
+subcategory,
+cost,
+COUNT(DISTINCT order_number) AS total_orders,
+SUM(sales_amount) AS total_sales,
+SUM(quantity) AS total_quantity,
+COUNT(DISTINCT customer_key) AS total_customers,
+MAX(order_date) AS last_sale_date,
+timestampdiff(MONTH, MIN(order_date), MAX(order_date)) AS lifespan,
+ROUND(AVG(CAST(sales_amount AS FLOAT) / NULLIF(quantity, 0)),1) AS avg_selling_price
+FROM base_query 
+GROUP BY 
+product_key,
+product_name,
+category,
+subcategory,
+cost)
+SELECT * ,
+timestampdiff(MONTH, last_sale_date, CURDATE()) AS recency_in_months,
+CASE
+	WHEN total_sales > 50000 THEN 'High-Performer'
+    When total_sales >= 10000 then 'Mid-Range'
+    ELSE 'Low-Performer'
+    END AS produc_segment,
+CASE
+	WHEN total_orders = 0 THEN 0
+    ELSE total_sales / total_orders
+END AS avg_order_revenue,
+CASE
+	WHEN lifespan = 0 THEN total_sales
+    ELSE total_sales / lifespan
+END AS avg_monthly_revenue
+FROM product_aggregations;
+
